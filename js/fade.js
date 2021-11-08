270c8d67170ac28203ad168d21c54e175f8bc690
Akash Yadav <67957388+Akashydv@users.noreply.github.com>
2021-11-08 12:25:04 +0530

    Add files via upload

--- js/fade.js
+++ js/fade.js
@@ -41,4 +41,44 @@ $(function () {
     $(window).trigger('scroll');
   });
   
-
+  $(function () {
+    if ($('.item-text-head').length) {
+        scrollAnimation();
+    }
+  
+    function scrollAnimation() {
+        $(window).scroll(function () {
+            $(".item-text-head").each(function () {
+                let position = $(this).offset().top,
+                    scroll = $(window).scrollTop(),
+                    windowHeight = $(window).height();
+  
+                if (scroll > position - windowHeight + 80) {
+                    $(this).addClass('text-focus-in');
+                }
+            });
+        });
+    }
+    $(window).trigger('scroll');
+  });
+  
+  $(function () {
+    if ($('.para').length) {
+        scrollAnimation();
+    }
+  
+    function scrollAnimation() {
+        $(window).scroll(function () {
+            $(".para").each(function () {
+                let position = $(this).offset().top,
+                    scroll = $(window).scrollTop(),
+                    windowHeight = $(window).height();
+  
+                if (scroll > position - windowHeight + 80) {
+                    $(this).addClass('scale-up-center');
+                }
+            });
+        });
+    }
+    $(window).trigger('scroll');
+  });
